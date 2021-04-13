af3722e0015d96d43af64c006733efbb3ea2d7d4
DavisLydon <78446652+DavisLydon@users.noreply.github.com>
2021-04-12 20:06:53 -0400

    Update admin.py

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -5,7 +5,7 @@ from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['name', 'user', 'email', 'year', 'wake_up', 'go_to_bed', 'how_clean', 'guests', 'more_introverted_or_extroverted', 'ideal_rent']}),
+        (None,               {'fields': ['name', 'user', 'email', 'year', 'wake_up', 'go_to_bed', 'how_clean', 'guests', 'more_introverted_or_extroverted', 'ideal_rent', 'pfp']}),
     ]
     list_display = ('email', 'year')
 
