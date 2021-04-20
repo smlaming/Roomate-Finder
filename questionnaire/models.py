f4dd507de1bfc69fb377f92d49a8977a1e241bd3
caroline-britt <crb8ua@virginia.edu>
2021-04-20 00:54:52 -0400

    Add validators for Event form

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.db import models
 from django.contrib.auth.models import User
+import re
+from django.core.exceptions import ValidationError
+from datetime import date
 
 # Create your models here.
 '''
@@ -47,11 +50,69 @@ class Question(models.Model):
     def get_user(self): # received help from Jude in OH to create this function / use it in views
         return self.user
 
+#https://georgexyz.com/django-model-form-validation.html
+def day_validator(value):
+    month = str(value.month)
+    day = str(value.day)
+    if len(month) < 2:
+        month = "0"+month
+    if len(day) < 2:
+        day = "0"+day
+    value_as_str = str(value.year)+"-"+str(month)+"-"+str(day)
+    today = date.today()
+    # https://stackoverflow.com/questions/14225608/python-how-to-use-regex-in-an-if-statement
+    '''
+    if not re.match(regex, value):
+        raise ValidationError(
+            ('%(value)s not a properly formatted date: please use YYYY-MM-DD'),
+            params={'value':value}
+        )
+    '''
+    #https://www.geeksforgeeks.org/python-program-to-print-current-year-month-and-day/#:~:text=In%20Python%2C%20in%20order%20to%20print%20the%20current,function%20of%20date%20class%20to%20fetch%20todays%20date.
+    #https://izziswift.com/how-do-i-validate-a-date-string-format-in-python/
+    if (int(value_as_str[0:4]) < int(today.year)):
+        raise ValidationError(
+            ('%(value)s not a valid year: please use this year or an upcoming year'),
+            params={'value':value}
+        )
+    elif (int(value_as_str[0:4]) == int(today.year)) and (int(value_as_str[5:7])< int(today.month)):
+        raise ValidationError(
+            ('%(value)s not a valid month: please use a month that has not already passed'),
+            params={'value':value}
+        )
+    elif (int(value_as_str[5:7]) == int(today.month)) and (int(value_as_str[8:10]) < int(today.day)):
+        raise ValidationError(
+            ('%(value)s not a valid day: please use a day that has not passed yet'),
+            params={'value':value}
+        )
+
+
+def time_validator(value):
+    #regex = '[0-9][0-9]:[0-9][0-9]'
+    hour = str(value.hour)
+    min = str(value.min)[0:2]
+    '''
+    if len(hour) < 2:
+        hour = "0" + hour
+    if len(min) < 2:
+        min = "0" + min
+    '''
+    if int(hour) < 0 or int(hour) > 23:
+        raise ValidationError(
+            ('%(value)s not a proper hour: please enter a valid hour'),
+            params={'value':value}
+        )
+    elif int(min) < 0 or int(min) > 59:
+        raise ValidationError(
+            ('%(value)s not a proper minute: please enter a proper minute value'),
+            params={'value':value}
+        )
+
 class Event(models.Model):
     summary = models.CharField(max_length=200)
     zoom_link = models.URLField(max_length=200, blank=True)
-    day = models.DateField(auto_now=False, help_text='YYYY-MM-DD') #YYYY-MM-DD HH:MM
-    start_time = models.TimeField(auto_now=False, help_text='HH:MM')
+    day = models.DateField(auto_now=False, help_text='YYYY-MM-DD', validators=[day_validator]) #YYYY-MM-DD HH:MM #
+    start_time = models.TimeField(auto_now=False, help_text='HH:MM', validators=[time_validator])
     duration = models.IntegerField(default=1)
     inviter = models.EmailField()
     invitee = models.EmailField()
